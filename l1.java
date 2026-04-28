d877ac11065ef21f1067054b3455706313236bb2
Deepak192511008 <192511008.simats@saveetha.com>
2026-04-28 19:24:30 +0530

    Update l1.java

--- l1.java
+++ l1.java
@@ -1,3 +1,52 @@
+//Full Diamond Pattern
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // 🔺 Upper pyramid
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // 🔻 Inverted pyramid
+        for(int i=n-1;i>=1;i--){
+
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+5
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Right Triangle
 import java.util.*;
 class Main 
